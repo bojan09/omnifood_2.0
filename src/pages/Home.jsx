27a8dd143a80c391f5b906ca3d8b4157438099ad
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-06-17 14:16:39 +0200

    Home page, work in progress

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,10 +6,26 @@ import Slider from "../components/Slider";
 const Home = () => {
   return (
     <div>
-      Home
-      {/*
+      {/* Left Container */}
+      <div>
+        <h1>A healthy meal delivered to your door, every single day</h1>
+        <p>
+          The smart 365-days-per-year food subscription that will make you eat
+          healthy again. Tailored to your personal tastes and nutritional needs.
+        </p>
+        {/*
     <Slider />
   */}
+        <div>
+          <button>
+            <a href="#howItWorks">Learn more</a>
+          </button>
+
+          <button>
+            <a href="#contact">Get Started</a>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
