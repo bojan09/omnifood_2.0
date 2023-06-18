6de961e9d0990b91a29567dd7a9ca257eb12c220
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-06-18 17:15:22 +0200

    added Contact & Footer pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,67 @@
 import React from "react";
 
+// assets
+import footerLogo from "../assets/omnifood-logo.png";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="my-[5rem] border-t-2 xs:w-[90vw] md:w-[80vw] mx-auto md:flex flex-row-reverse justify-center items-center md:gap-[5rem]">
+      {/* Top container */}
+      <div className="flex justify-center my-[3rem] gap-5 md:gap-[5rem]">
+        {/* Account - container */}
+        <div className="font-light text-sm flex flex-col gap-4">
+          <h1 className="mb-5 font-normal">Account</h1>
+          <p>Create account</p>
+          <p>Sign in</p>
+          <p>iOS App</p>
+          <p>Android App</p>
+        </div>
+
+        {/* Company - container */}
+        <div className="font-light text-sm flex flex-col gap-4">
+          <h1 className="mb-5 font-normal">Company</h1>
+          <p>About Omnifood</p>
+          <p>For Business</p>
+          <p>Cooking partners</p>
+          <p>Carrers</p>
+        </div>
+
+        {/* Resources - container */}
+        <div className="font-light text-sm flex flex-col gap-4">
+          <h1 className="mb-5 font-normal">Resources</h1>
+          <p>Recipe directory</p>
+          <p>Help center</p>
+          <p>Privacy & terms</p>
+        </div>
+      </div>
+
+      {/* Bottom container */}
+      <div className="flex gap-8 md:gap-[5rem] md:mb-[3rem] ">
+        {/* Logo & Copyright - container */}
+        <a href="#" className="w-[50vw] md:w-[12vw]">
+          <img src={footerLogo} alt="logo" className="mb-[4rem]" />
+          <p className="font-light text-sm flex flex-col gap-4">
+            Copyright © 2023 by Omnifood, Inc. <br />
+            All rights reserved.
+          </p>
+        </a>
+        {/* Contact us - container */}
+        <div>
+          <h1 className="mb-5 font-normal">Contact us</h1>
+          <div className="font-light text-sm flex flex-col gap-4">
+            <p>
+              623 Harrison St., 2nd Floor <br />
+              San Francisco, CA 94107
+            </p>
+            <p>
+              415-201-6370 <br />
+              hello@omnifood.com
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,85 @@
 import React from "react";
 
+import formImage from "../assets/bowls.jpg";
+
 const Contact = () => {
-  return <div id="contact">Contact</div>;
+  return (
+    <div
+      id="contact"
+      className=" xs:w-[90vw] md:w-[82vw] lg:w-[60vw] lg:h-[60vh] flex md:flex-row xs:flex-col-reverse rounded-lg mx-auto bg-gradient-to-br from-primaryColor to-shadeColor"
+    >
+      <div className="m-6">
+        {/* Left container - Form*/}
+        <h1 className="text-3xl font-medium my-6">
+          Get your first meal for free!
+        </h1>
+        <p className="font-light text-md my-6">
+          Healthy, tasty and hassle-free meals are waiting for you. Start eating
+          well today. You can cancel or pause anytime. And the first meal is on
+          us!
+        </p>
+        <form className="font-sm text-lg">
+          <div className="md:flex items-center justify-center gap-2">
+            <div className="">
+              <label className="ml-2">Full Name</label>
+              <input
+                className="p-2 my-3 w-full rounded-lg bg-primaryBackground"
+                type="text"
+                placeholder="John Smith"
+              />
+            </div>
+
+            <div className="xs:my-3 md:m-0">
+              <label className="ml-2">Email adress</label>
+              <input
+                className="p-2 my-3 w-full rounded-lg bg-primaryBackground"
+                type="email"
+                placeholder="john@smith.com"
+              />
+            </div>
+          </div>
+
+          <div className="md:flex gap-3 items-center justify-center">
+            <div className="mt-3 flex-1">
+              <label className="ml-2">Where did you hear from us?</label>
+              <select
+                className="p-2 my-3 w-full rounded-lg bg-primaryBackground"
+                name="Where did you hear from us?"
+                placeholder="Please choose one option"
+                required
+              >
+                <option value="" disabled defaultChecked hidden>
+                  Please choose one option:
+                </option>
+                <option value="friends and family">Friends and family</option>
+                <option value="twitter">Twitter</option>
+                <option value="instagram">Instagram</option>
+                <option value="facebook">Facebook</option>
+                <option value="other">Other</option>
+              </select>
+            </div>
+            <div className="flex-1 flex justify-center items-center mt-9 text-center">
+              <button
+                className=" bg-darkColor  rounded-md hover:bg-shadeColor hover:border-[2px] border-yellow-100 text-white font-medium transition-all duration-200 ease-in-out py-[.4rem] xs:px-6 lg:px-[5rem] md:px-[3rem] md:mt-[2rem] lg:mt-0"
+                type="submit"
+              >
+                Sign up now
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+
+      {/* Right container - Image */}
+      <div className="overflow-hidden">
+        <img
+          src={formImage}
+          alt="formImage"
+          className="xs:rounded-t-lg md:rounded-tl-none md:rounded-tr-lg md:rounded-br-lg h-fit bg-cover bg-center"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
